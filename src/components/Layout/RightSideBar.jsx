ad1fd649191642e177d69b7879a07979fb14dc4e
iamjunaidjutt <engr.muhammadjunaid@hotmail.com>
2023-01-19 04:24:52 +0500

    Add Partition component and complete the AboutMe section

--- src/components/Layout/RightSideBar.jsx
+++ src/components/Layout/RightSideBar.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import NavBar from '../NavBar/NavBar'
 import HeroSection from '../Sections/HeroSection'
+import AboutMe from '../Sections/AboutMe'
+import Partition from '../UI/Partition'
+import Skills from '../Sections/Skills'
 
 const RightSideBar = () => {
   return (
     <div className='w-full ml-24'>
       <NavBar/>
       <HeroSection />
+      <Partition />
+      <AboutMe />
+      <Partition />
+      <Skills />
     </div>
   )
 }
---
+++ src/components/UI/Partition.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Partition = () => {
+  return (
+    <div className='border-t border-myRed w-4/5 m-auto opacity-50'></div>
+  )
+}
+
+export default Partition
